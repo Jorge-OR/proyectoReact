c0f6a23e3b118774240f53064436997eb9acf160
Jorge <n33uj4hi22@gmail.com>
2025-11-19 14:43:18 +0100

    Agregar cambios para UT04_ACT1c

--- src/components/Form.js
+++ src/components/Form.js
@@ -12,29 +12,29 @@ class Form extends React.Component {
 
     render() {
         return (
-            <div>
-                <h2> Registrar incidencias </h2>
+            <div className="card mx-auto">
+                <h2 className="card-tittle mb-4 text-center"> Registrar incidencias </h2>
                 <form onSubmit={this.envioFormulario}>
                     
                       {/*Usuario*/}
                     <div class="elemento-form">
-                        <label> Usuario </label>
-                        <input type="text" name="usuario" placeholder="Ej: María" required/>
+                        <label className="mb-3 form-label"> Usuario </label>
+                        <input className="mb-3 form-control" type="text" name="usuario" placeholder="Ej: María" required/>
                     </div>
                     {/*Título*/}
                     <div class="elemento-form">
-                        <label> Título </label>
-                        <input type="text" name="titulo" placeholder="Ej: No funciona ratón del ordenador" required/>
+                        <label className="mb-3 form-label"> Título </label>
+                        <input className="mb-3 form-control" type="text" name="titulo" placeholder="Ej: No funciona ratón del ordenador" required/>
                     </div>
                     {/*Descripción*/}
                     <div class="elemento-form">
-                        <label> Descripción </label>
-                        <textarea name="descripcion" placeholder="Escribe la descripción de la incidencia" required/>
+                        <label className="mb-3 form-label"> Descripción </label>
+                        <textarea className="mb-3 form-control" name="descripcion" placeholder="Escribe la descripción de la incidencia" required/>
                     </div>
                     {/*Categoría*/}
                     <div class="elemento-form">
-                        <label> Categoría </label>
-                        <select name="categoria" required>
+                        <label className="mb-3 form-label"> Categoría </label>
+                        <select className="mb-3 form-control" name="categoria" required>
                             <option value="">Seleccionar...</option>
                             <option>Hardware</option>
                             <option>Software</option>
@@ -45,8 +45,8 @@ class Form extends React.Component {
                     </div>
                     {/*Urgencia*/}
                     <div class="elemento-form">
-                        <label> Nivel de urgencia </label>
-                        <select name="nivel" required>
+                        <label className="mb-3 form-label"> Nivel de urgencia </label>
+                        <select className="mb-3 form-control" name="nivel" required>
                             <option value="">Seleccionar...</option>
                             <option>Alta</option>
                             <option>Media</option>
@@ -56,11 +56,11 @@ class Form extends React.Component {
                     
                     {/*Ubicación*/}
                     <div class="elemento-form">
-                        <label> Ubicación </label>
-                        <input type="text" name="ubicacion" placeholder="Ej: B205" required/>
+                        <label className="mb-3 form-label"> Ubicación </label>
+                        <input className="mb-3 form-control" type="text" name="ubicacion" placeholder="Ej: B205" required/>
                     </div>
                     {/*Botón*/}
-                    <button type="submit" className="elemento-form-button"> Registrar </button>
+                    <button type="submit" className="btn btn-outline-danger btn-lg mx-auto d-grid"> Registrar </button>
 
                 </form>
             </div>
--- src/components/main/App.js
+++ src/components/main/App.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react';
 import Header from '../header/Header';
 import Footer from '../footer/Footer';
-import MiLista from '../MiLista';
+import IncidentList from '../IncidentList';
 import Form from '../Form'
+import Foto from '../../img/Incidencias.png';
 
 
-class App extends React.Component {
-  state = {incidencias: [
+function App () {
+ const [incidencias, setIncidencia] = useState( [
      {
             id_incidencia: 1,
             id_usuario: "Pedro",
@@ -21,7 +21,7 @@ class App extends React.Component {
         },
 
         {
-            id_incidencia: 1,
+            id_incidencia: 2,
             id_usuario: "Lara",
             titulo: "Impresora sin conexión",
             descripcion: "Impresora sin conexión",
@@ -33,7 +33,7 @@ class App extends React.Component {
         },
 
         {
-            id_incidencia: 1,
+            id_incidencia: 3,
             id_usuario: "Sara",
             titulo: "Ordenador de secretaría no enciende",
             descripcion: "Ordenador de secretaría no enciende",
@@ -45,7 +45,7 @@ class App extends React.Component {
         },
 
         {
-            id_incidencia: 1,
+            id_incidencia: 4,
             id_usuario: "Naty",
             titulo: "Wifi no disponible",
             descripcion: "Wifi no disponible",
@@ -57,7 +57,7 @@ class App extends React.Component {
         },
         
         {
-            id_incidencia: 1,
+            id_incidencia: 5,
             id_usuario: "Merche",
             titulo: "Pantalla táctil no responde",
             descripcion: "Pantalla táctil no responde",
@@ -69,19 +69,19 @@ class App extends React.Component {
         },
 
         {
-            id_incidencia: 1,
+            id_incidencia: 6,
             id_usuario: "José",
             titulo: "Ratón y teclado dañados",
             descripcion: "Ratón y teclado dañados",
             categoria: "Hardware",
             nivel_urgencia: "Alta",
             fecha_registro: "2025-10-20",
-            estado: "Baja",
+            estado: "Abierta",
             ubicacion: "B204"
         },
 
         {
-            id_incidencia: 1,
+            id_incidencia: 7,
             id_usuario: "María",
             titulo: "Altavoces sin sonido",
             descripcion: "Altavoces sin sonido",
@@ -93,7 +93,7 @@ class App extends React.Component {
         },
 
         {
-            id_incidencia: 1,
+            id_incidencia: 8,
             id_usuario: "Blanca",
             titulo: "Servidor se reinicia",
             descripcion: "Servidor se reinicia",
@@ -105,7 +105,7 @@ class App extends React.Component {
         },
 
          {
-            id_incidencia: 1,
+            id_incidencia: 9,
             id_usuario: "Carolina",
             titulo: "Cable HDMI roto",
             descripcion: "Cable HDMI roto",
@@ -115,43 +115,50 @@ class App extends React.Component {
             estado: "Abierta",
             ubicacion: "B104"
         }
-  ]
-}
+  ])
 
-    agregarIncidencia = (id_nuevo,usuario_nuevo,titulo_nuevo,descripcion_nuevo,categoria_nuevo,urgencia_nuevo,
-                            fecha_nuevo,estado_nuevo,ubicacion_nuevo)=> {
+
+   const agregarIncidencia = (usuario_nuevo,titulo_nuevo,descripcion_nuevo,categoria_nuevo,urgencia_nuevo,
+                            ubicacion_nuevo)=> {
+                const fecha = new Date();
+                const year = fecha.getFullYear();
+                const month = String(fecha.getMonth() + 1).padStart(2, '0'); // meses 0-11
+                const day = String(fecha.getDate()).padStart(2, '0');
+                const fechaFormateada = `${year}-${month}-${day}`;
         const nueva_incidencia={
-            id_incidencia: id_nuevo,
+            id_incidencia: incidencias.length+1,
             id_usuario: usuario_nuevo,
             titulo: titulo_nuevo,
             descripcion: descripcion_nuevo,
             categoria: categoria_nuevo,
             nivel_urgencia: urgencia_nuevo,
-            fecha_registro: fecha_nuevo,
-            estado: estado_nuevo,
+            fecha_registro: fechaFormateada,
+            estado: "Abierta",
             ubicacion: ubicacion_nuevo
         }
         console.log("Nueva incidencia",nueva_incidencia);
-        this.setState({incidencias:[...this.state.incidencias, nueva_incidencia]})
+        setIncidencia([...incidencias, nueva_incidencia])
     }
-render(){
+
   return (
-    <>
+    <div className='card' style={{backgroundImage: `url(${Foto})`, backgroundSize: "cover", 
+                    backgroundRepeat: "no repeat"}}>
     <Header/>
-    <h2>Mi aplicacion</h2>
-    <div className="contenedor-incidencias">
-        <main>
+    <h2 className='mb-4 text-center'>Mi aplicacion</h2>
+    <div className="container-fluid mt-4 row">
+        <main className='col-md-8'>
             <p>Esta aplicacion muestra el contenido almacenado de mi app</p>
-            <MiLista incidencias={this.state.incidencias}/>
+            <IncidentList incidencias={incidencias}/>
         </main>
-        <aside>
-            <Form agregarIncidencia={this.agregarIncidencia}/>
+        <aside className='col-md-4'>
+            <Form agregarIncidencia={agregarIncidencia}/>
         </aside>
     </div>
     <Footer/>
-    </>
+    </div>
   );
   }
-}
+
+
 
 export default App;
