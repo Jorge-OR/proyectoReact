7ce1d763b35cd86c8c80f5f6f795048a3cae8dfb
Jorge <n33uj4hi22@gmail.com>
2026-05-18 13:58:29 +0200

    Agregar cambios para UT07_ACT1

---
+++ src/components/main/Menu.js
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+
+function Menu(props) {
+    return (
+        <div>
+            <nav>
+                <ul>
+                    <li><Link to="/">Inicio</Link></li>
+                    <li><Link to="/ver">Ver incidencias</Link></li>
+                    <li><Link to="/registrar">Registrar incidencias</Link></li>
+                    {props.usuarioLogin && (props.usuarioLogin.rol.nombre_rol == "admin") ? 
+                        <li><Link to="/gestion-usuarios">Gestión de Usuarios</Link></li>: null}
+                    
+                </ul>
+            </nav>
+        </div>
+    );
+}
+
+export default Menu;
--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/main/App';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { BrowserRouter } from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
 
 
